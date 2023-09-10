b1d3b1f80ba0e995da2ad43f24c3c4a4a51d1347
Victor <remmark999@gmail.com>
2023-09-10 14:21:30 +0500

    Init commit

---
+++ configs/null-ls.lua
@@ -0,0 +1,28 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.eslint_d,
+  b.code_actions.eslint_d,
+  b.diagnostics.eslint_d,
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+  b.diagnostics.cppcheck,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
